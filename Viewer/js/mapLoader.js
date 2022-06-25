d07d95e5e21570ff6626db58e8a0fc604ea7acdc
FukudaKento <fuku_0000@outlook.jp>
2022-06-26 01:22:57 +0900

    leafretでのrange表示用ライブラリの追加

--- Viewer/js/mapLoader.js
+++ Viewer/js/mapLoader.js
@@ -1,11 +1,10 @@
+var circles = [];
 function mapLoad() {
 
     var testDatas = GetJson();
-
     var map = InitializeMap();
     
     var makers = [];
-    var circles = [];
     for (let i = 0; i < testDatas.length; i++) {
         var marker = AddRefarenceStationMarker(testDatas[i], map);
         var circle = AddRefarenceStationCircle(testDatas[i], map);
@@ -14,8 +13,16 @@ function mapLoad() {
     }
 }
 
-function toggleFunction(bool) {
-    console.log(bool);
+function toggleFunction(_radius) {
+    ChangeCircles(_radius * 1000);
+}
+
+function ChangeCircles(_radius)
+{
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].setRadius(_radius);
+    }
+
 }
 
 function GetJson()
@@ -37,33 +44,100 @@ function GetJson()
 
 function InitializeMap()
 {
+    AddSlider();
+
     var map = L.map('map');
     map.setView([35.40, 136], 5);
     L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
     }).addTo(map);
-    
+
+    var control = L.control.range({
+        orient: 'vertical',
+        value: 100
+    });
+
+    control.on('change input', function(e) {
+        console.log(e.value);
+        layer.setOpacity(e.value / 100);
+    })
+
+    map.addControl(control);
+
     // 距離スケール
     L.control.scale({position: 'bottomright', imperial: false }).addTo(map);
-
-    var command = L.control({position: 'topright'});
-    command.onAdd = function (map) {
-        var div = L.DomUtil.create('div');
-        div.innerHTML = `
-        <div class="leaflet-control-layers leaflet-control-layers-expanded">
-          <form>
-            <input class="leaflet-control-layers-overlays" id="command" 
-              onclick=toggleFunction(this.checked) type="checkbox">
-              Toggle
-            </input>
-          </form>
-        </div>`; 
-        return div;
-    };
-    command.addTo(map);
-
+    
 
     return map;
+}
+
+function AddSlider()
+{
+    console.log("aaa");
+    L.Control.Range = L.Control.extend({
+        options: {
+            position: 'topleft',
+            min: 0,
+            max: 100,
+            value: 0,
+            step: 1,
+            orient: 'horizontal',
+            iconClass: 'leaflet-range-icon',
+            icon: true
+        },
+        
+        onAdd: function(map) {
+            var container = L.DomUtil.create('div', 'leaflet-range-control leaflet-bar ' + this.options.orient);
+            if (this.options.icon) {
+              L.DomUtil.create('span', this.options.iconClass, container);
+            };
+            var slider = L.DomUtil.create('input', '', container);
+            slider.type = 'range';
+            slider.setAttribute('orient', this.options.orient);
+            slider.min = this.options.min;
+            slider.max = this.options.max;
+            slider.step = this.options.step;
+            slider.value = this.options.value;
+    
+            L.DomEvent.on(slider, 'mousedown mouseup click touchstart', L.DomEvent.stopPropagation);
+    
+            /* IE11 seems to process events in the wrong order, so the only way to prevent map movement while dragging the
+             * slider is to disable map dragging when the cursor enters the slider (by the time the mousedown event fires
+             * it's too late becuase the event seems to go to the map first, which results in any subsequent motion
+             * resulting in map movement even after map.dragging.disable() is called.
+             */
+            L.DomEvent.on(slider, 'mouseenter', function(e) {
+                map.dragging.disable()
+            });
+            L.DomEvent.on(slider, 'mouseleave', function(e) {
+                map.dragging.enable();
+            });
+    
+            L.DomEvent.on(slider, 'change', function(e) {
+                this.fire('change', {value: e.target.value});
+            }.bind(this));
+    
+            L.DomEvent.on(slider, 'input', function(e) {
+                this.fire('input', {value: e.target.value});
+            }.bind(this));
+    
+            this._slider = slider;
+            this._container = container;
+    
+            return this._container;
+        },
+    
+        setValue: function(value) {
+            this.options.value = value;
+            this._slider.value = value;
+        },
+    });
+    
+    L.Control.Range.include(L.Evented.prototype)
+    
+    L.control.range = function (options) {
+      return new L.Control.Range(options);
+    };
 }
 
 function AddRefarenceStationMarker(_referenceStationData, map)
