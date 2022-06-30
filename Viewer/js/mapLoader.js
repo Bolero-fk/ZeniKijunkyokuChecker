959399ea9007750f86f949eabc1fbb7dd3c1b93c
FukudaKento <fuku_0000@outlook.jp>
2022-06-30 22:01:26 +0900

    処理速度の改善

--- Viewer/js/mapLoader.js
+++ Viewer/js/mapLoader.js
@@ -37,7 +37,9 @@ function GetJson()
 
 function InitializeMap()
 {
-    var map = L.map('map');
+    var map = L.map('map',{
+        preferCanvas:true
+    });
     map.setView([35.40, 136], 5);
     L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
